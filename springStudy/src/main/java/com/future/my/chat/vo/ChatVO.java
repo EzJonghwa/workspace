6cdd04b497e4f154def4f019b3e8341eaae30c43
EzJonghwa <jj2bae@naver.com>
2024-10-15 17:47:02 +0900

    2024-10-15

--- springStudy/src/main/java/com/future/my/chat/vo/ChatVO.java
+++ springStudy/src/main/java/com/future/my/chat/vo/ChatVO.java
@@ -9,55 +9,67 @@ public class ChatVO {
 	private String profileImg;
 	private String chatMsg;
 	private String sendDate;
-	
+
 	public int getChatNo() {
 		return chatNo;
 	}
+
 	public void setChatNo(int chatNo) {
 		this.chatNo = chatNo;
 	}
+
 	public String getMemId() {
 		return memId;
 	}
+
 	public void setMemId(String memId) {
 		this.memId = memId;
 	}
+
 	public String getMemNm() {
 		return memNm;
 	}
+
 	public void setMemNm(String memNm) {
 		this.memNm = memNm;
 	}
+
 	public int getRoomNo() {
 		return roomNo;
 	}
+
 	public void setRoomNo(int roomNo) {
 		this.roomNo = roomNo;
 	}
+
 	public String getProfileImg() {
 		return profileImg;
 	}
+
 	public void setProfileImg(String profileImg) {
 		this.profileImg = profileImg;
 	}
+
 	public String getChatMsg() {
 		return chatMsg;
 	}
+
 	public void setChatMsg(String chatMsg) {
 		this.chatMsg = chatMsg;
 	}
+
 	public String getSendDate() {
 		return sendDate;
 	}
+
 	public void setSendDate(String sendDate) {
 		this.sendDate = sendDate;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "ChatVO [chatNo=" + chatNo + ", memId=" + memId + ", memNm=" + memNm + ", roomNo=" + roomNo
 				+ ", profileImg=" + profileImg + ", chatMsg=" + chatMsg + ", sendDate=" + sendDate + "]";
 	}
-	
-	
+
 }
